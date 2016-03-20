cb4cfc7481bc957c0a1152921bd18e9508e2ec22
NEGU93 <joseagustin.barra@gmail.com>
2016-03-20 01:20:28 -0300

    Version 1.1 - Verification before running and a readme instructions

--- printerSpooler.bat
+++ printerSpooler.bat
@@ -1,11 +1,21 @@
 @ECHO OFF
 ECHO --------------- Printer Spooler made by NEGU ---------------
 ECHO J. A. BARRACHINA
-ECHO ..
-ECHO .
-NET STOP Spooler /y
-del /q C:\Windows\System32\spool\PRINTERS
-NET START Spooler /y
+
+:Main
+ECHO This will delete all documents in the printer spooler
+set /p D="Are you sure you want to continue? Y/N: "
+
+IF %D%==Y (
+	NET STOP Spooler /y
+	ECHO deleting files of the printer spooler
+	del /q C:\Windows\System32\spool\PRINTERS
+	NET START Spooler /y
+) ELSE (
+	IF NOT %D%==N (
+		GOTO Main
+	)
+)
 
 ECHO Done...
 PAUSE
